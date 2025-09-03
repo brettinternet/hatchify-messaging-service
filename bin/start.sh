8444a949edee1d112949ea2e3d84e2417c992464
brett <20871604+brettinternet@users.noreply.github.com>
2025-09-03 16:11:58 -0600

    remove start files

--- bin/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo "Starting the application..."
-echo "Environment: ${ENV:-development}"
-
-# Add your application startup commands here
-echo "Application started successfully!" 
